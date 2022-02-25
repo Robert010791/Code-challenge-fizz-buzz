ac5f0993253c8ad997089404494dc2d205ab99b3
Robert Robbins <robertrobbins321@msn.om>
2022-02-24 18:40:37 -0700

    Initial commit

---
+++ maxOfTwo.js
@@ -0,0 +1,10 @@
+x = 1
+y = 2
+
+if (x < y){
+    console.log("Y is the greatest of all time!!!")
+} else if(x === y){
+    console.log("They have identical values!!!")
+} else {
+    console.log("X is the largest number of them all!!!")
+}
